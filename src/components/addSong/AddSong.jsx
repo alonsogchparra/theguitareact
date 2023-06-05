be2c5d8d6285e2954e071e2bd7ae589b2b000784
Alonso Parra <alonsogparra@gmail.com>
2023-06-05 15:39:13 -0400

    AddSong refactored with AddForm

--- src/components/addSong/AddSong.jsx
+++ src/components/addSong/AddSong.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Box,
-  Button,
   Container,
   CssBaseline,
   responsiveFontSizes,
-  TextField,
   ThemeProvider,
   Typography,
 } from '@mui/material';
@@ -15,6 +12,7 @@ import {
   useFetchSongQuery,
   useUpdateSongMutation,
 } from '../../features/songs/songsApi';
+import { AddForm } from './AddForm';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/auth/userSlice';
@@ -91,86 +89,16 @@ export const AddSong = () => {
               {id ? 'Edit Song' : 'Add Song'}
             </Typography>
 
-            <Box component='form' marginTop={5}>
-              <div>
-                <TextField
-                  id='song-title'
-                  label='Song Title'
-                  variant='standard'
-                  fullWidth
-                  margin='normal'
-                  autoComplete='off'
-                  name='title'
-                  value={title}
-                  className='gr_email_input'
-                  onChange={handleChange}
-                />
-                <TextField
-                  id='artist-name'
-                  label='Artist/Band’s name'
-                  variant='standard'
-                  fullWidth
-                  margin='normal'
-                  autoComplete='off'
-                  name='artist'
-                  value={artist}
-                  className='gr_email_input'
-                  onChange={handleChange}
-                />
-                <TextField
-                  id='artist-name'
-                  label='Beat Per Minute (Optional)'
-                  variant='standard'
-                  fullWidth
-                  margin='normal'
-                  autoComplete='off'
-                  name='beat_per_minute'
-                  value={beat_per_minute}
-                  className='gr_email_input'
-                  onChange={handleChange}
-                  InputLabelProps={{ shrink: true }}
-                />
-                <TextField
-                  id='artist-name'
-                  label='Beats per measure (Optional)'
-                  variant='standard'
-                  fullWidth
-                  margin='normal'
-                  autoComplete='off'
-                  name='beats_per_measure'
-                  value={beats_per_measure}
-                  className='gr_email_input'
-                  onChange={handleChange}
-                  InputLabelProps={{ shrink: true }}
-                />
-                <TextField
-                  id='additional-info'
-                  label='Extra Info (Optional)'
-                  multiline
-                  fullWidth
-                  rows={4}
-                  margin='normal'
-                  style={{ marginTop: '20px' }}
-                  autoComplete='off'
-                  name='extraInfo'
-                  value={extraInfo}
-                  className='gr_extra_info'
-                  onChange={handleChange}
-                />
-              </div>
-
-              <Button
-                fullWidth
-                variant='contained'
-                style={{ marginTop: '30px' }}
-                className='gr_add_button'
-                onClick={(e) => addEditSongHandler(e)}
-              >
-                <Typography component='div' variant='h4'>
-                  {id ? 'UPDATE' : 'ADD'}
-                </Typography>
-              </Button>
-            </Box>
+            <AddForm
+              id={id}
+              title={title}
+              artist={artist}
+              beat_per_minute={beat_per_minute}
+              beats_per_measure={beats_per_measure}
+              extraInfo={extraInfo}
+              handleChange={handleChange}
+              addEditSongHandler={addEditSongHandler}
+            />
           </Grid>
         </Container>
       </ThemeProvider>
